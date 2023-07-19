cf511bf2573118bcd319577f5e12bfd63a5e9e61
jiongcheUSC <97581480+jiongcheUSC@users.noreply.github.com>
2023-07-19 17:51:54 +0800

    added eureka

--- src/main/java/com/example/messageservice/MessageServiceApplication.java
+++ src/main/java/com/example/messageservice/MessageServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.messageservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class MessageServiceApplication {
